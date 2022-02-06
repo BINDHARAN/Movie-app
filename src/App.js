f0dda6ff1eed35aa9e8c1bd9636dba3eb1792dd0
BINDHARAN <bindharan885@gmail.com>
2022-02-07 01:17:03 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import "./App.css";
 import { useState } from "react";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Switch from '@mui/material/Switch';
 import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
+import { Switch, Route, Link } from "react-router-dom";
+import { drawerClasses } from "@mui/material";
+import { TicTacToe } from "./TicTacToe";
+
+
+
 
 
 export default function App() {
@@ -83,67 +88,116 @@ export default function App() {
   };
   return (
     <div className="App">
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="navbar-brand">
-          
-          <img
-            src="https://cdn4.iconfinder.com/data/icons/socialmediaicons_v120/48/imdb.png"
-            className="logo"
-            alt="logo"
-          ></img>
-          <span className="title">IMDb</span>
-        </div>
-      </nav>
 
-      <div className="content">
-        <h4 className="text-dark font-italic "> HELLO!</h4>
-        <p className="lead">
-          Here are IMDb "Top 5" Movies(Sorted by IMDb Rating Descending)...
-        </p>
+
+
+      <div className="list">
+        <ul>
+          <li>
+            {/* Change the url bar but dont refresh */}
+            <Link to="/movies" >Movies</Link>
+          </li>
+          <li>
+            <Link to="/color-game">Color Game</Link>
+          </li>
+          {/* <li>
+            <Link to="/fun">Wowwww</Link>
+          </li> */}
+          <li>
+            <Link to="/TicTacToe">TicTacToe</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+
+        </ul>
       </div>
 
-      <div className="row">
-        {movies.map((nm) => (
-          <Movie
-            name={nm.name}
-            profile={nm.profile}
-            rating={nm.rating}
-            description={nm.description}
-            director={nm.director}
-            stars={nm.stars}
-          />
-        ))}
-      </div>
-      <hr></hr>
+      <Switch>
+        {/* Each route is case, eg. - case '/about': */}
+        <Route path="/movies">
+          {/* Match url display the below component */}
 
-      <div className="content">
-        <h4 className="text-dark font-italic "> HELLO!</h4>
-        <p className="lead1">
-          
-          If you want to add 6th rating movie to the list above, just visit our
-          official website
-          (https://www.imdb.com/search/title/?groups=top_250&sort=user_rating).
-          And enter the required details in the below input...<i>ThanK YoU ❣</i>
-        </p>
-      </div>
+          <nav className="navbar navbar-dark bg-dark">
+            <div className="navbar-brand">
 
-      <div className="movie_form">
-      <TextField id="outlined-basic" label="Movie Poster(url)" variant="outlined" onChange={(event) => setMoviePoster(event.target.value)} />
-      <TextField id="outlined-basic" label="Movie Name" variant="outlined" onChange={(event) => setMovieName(event.target.value)} />
-      <TextField id="outlined-basic" label="Movie Rating" variant="outlined" onChange={(event) => setMovieRating(event.target.value)} />
-      <TextField id="outlined-basic" label="Movie Description" variant="outlined" onChange={(event) => setMovieDes(event.target.value)} />
-      <TextField id="outlined-basic" label="Movie Director" variant="outlined" onChange={(event) => setMovieDirector(event.target.value)} />
-      <TextField id="outlined-basic" label="Movie Stars/Actors" variant="outlined" onChange={(event) => setMovieStars(event.target.value)} />
-     
-      <Button variant="contained" className="button" onClick={addMovie}>   Add Movie</Button>
-      </div>
-    
-     
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/socialmediaicons_v120/48/imdb.png"
+                className="logo"
+                alt="logo"
+              ></img>
+              <span className="title">IMDb</span>
+            </div>
+          </nav>
+
+          <div className="content">
+            <h4 className="text-dark font-italic "> HELLO!</h4>
+            <p className="lead">
+              Here are IMDb "Top 5" Movies(Sorted by IMDb Rating Descending)...
+            </p>
+          </div>
+
+          <div className="row">
+            {movies.map(({ name, profile, rating, description, director, stars }, index) => (
+              <Movie
+                key={index}
+                name={name}
+                profile={profile}
+                rating={rating}
+                description={description}
+                director={director}
+                stars={stars}
+              />
+            ))}
+          </div>
+          <hr></hr>
+
+          <div className="content">
+            <h4 className="text-dark font-italic "> HELLO!</h4>
+            <p className="lead1">
+
+              If you want to add 6th rating movie to the list above, just visit our
+              official website
+              (https://www.imdb.com/search/title/?groups=top_250&sort=user_rating).
+              And enter the required details in the below input...<i>ThanK YoU ❣</i>
+            </p>
+          </div>
+
+          <div className="movie_form">
+            <TextField id="outlined-basic" label="Movie Poster(url)" variant="outlined" onChange={(event) => setMoviePoster(event.target.value)} />
+            <TextField id="outlined-basic" label="Movie Name" variant="outlined" onChange={(event) => setMovieName(event.target.value)} />
+            <TextField id="outlined-basic" label="Movie Rating" variant="outlined" onChange={(event) => setMovieRating(event.target.value)} />
+            <TextField id="outlined-basic" label="Movie Description" variant="outlined" onChange={(event) => setMovieDes(event.target.value)} />
+            <TextField id="outlined-basic" label="Movie Director" variant="outlined" onChange={(event) => setMovieDirector(event.target.value)} />
+            <TextField id="outlined-basic" label="Movie Stars/Actors" variant="outlined" onChange={(event) => setMovieStars(event.target.value)} />
+
+            <Button variant="contained" className="button" onClick={addMovie}>   Add Movie</Button>
+          </div>
+        </Route>
+
+        <Route path="/color-game">
+          <AddColor />
+        </Route>
+        <Route path="/TicTacToe">
+          <TicTacToe />
+        </Route>
+
+        <Route path="/">
+          < Msg />
+        </Route>
+
+
+      </Switch>
+
     </div>
   );
 }
 
-function Movie({ name, profile, rating,description, director, stars }) {
+function Movie({ name, profile, rating, description, director, stars }) {
+  const styles = {
+    color: rating > 9 ? "green" : "red"
+  }
   return (
     <div className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4">
       <div className="col mb-4">
@@ -153,11 +207,11 @@ function Movie({ name, profile, rating,description, director, stars }) {
             <h5 className="card-title text-center">{name}</h5>
             <p className="card-text">
               <span className="h5"> Rating : </span>
-              <span className="pfont"> ⭐{rating}</span>
+              <span className="pfont" style={styles}> ⭐{rating}</span>
             </p>
 
             <p className="card-text">
-             <Des props= {description} />
+              <Des props={description} />
             </p>
             <p className="card-text">
               <span className="h5"> Director: </span>
@@ -174,15 +228,17 @@ function Movie({ name, profile, rating,description, director, stars }) {
     </div>
   );
 }
-const Des= ({props}) => {
+const Des = ({ props }) => {
   const [showText, setShowText] = useState(false);
   return (
-    <div>
-        {/* <Button variant="contained"  className="toggle-button" >    </Button> */}
-        <div> <Switch defaultChecked onClick={() => setShowText(!showText)}  /><span className="h5"> Description: </span> </div>
-         
-      {showText && <p className="pfont"> {props} </p>}
-   
+    <div >
+
+      <div className="description">
+        <span > Description: </span>  <IconButton color="primary" onClick={() => setShowText(!showText)} > <span className="primary">{showText ? "🔼" : "🔽"}</span>
+        </IconButton>
+      </div>
+      {showText ? <p className="pfont"> {props}  </p> : ""}
+
     </div>
   );
 };
@@ -192,7 +248,7 @@ function Counter() {
   const [like, setLike] = useState(0);
   const [unlike, setunLike] = useState(0);
   return (
-    <div className="buttongroup">
+    <div className="button-group">
       <IconButton color="primary" className="btn1"
         onClick={() => setLike(like + 1)}>
         <Badge badgeContent={like} color="secondary">
@@ -208,7 +264,55 @@ function Counter() {
 
       </IconButton>
 
-  
+
     </div>
   );
+
 }
+
+function AddColor() {
+  const [color, setColor] = useState("pink")
+
+  const sytles = {
+    backgroundColor: color
+  }
+
+  const [colorlist, setColorList] = useState(["red", "teal", "skyblue"])
+  return (
+    <div className="total-colorbox">
+      <div className="coloritems">
+
+        <input style={sytles} value={color} className="colorbox-input" placeholder="Enter a color" onChange={(event) => setColor(event.target.value)} />
+        <button  onClick={() => setColorList([...colorlist, color])  }className="color-btn" >Add Color</button>
+
+      </div>
+      {colorlist.map((clr, index) => <ColorBox key={index} color={clr} />)}
+    </div>
+  )
+
+}
+
+
+function ColorBox({ color }) {
+  const styles = {
+    backgroundColor: color,
+    height: "40px",
+    width: "510px",
+    marginTop: "10px",
+    borderRadius: "10px"
+
+  }
+  return (<div className="addcolors">
+    <div style={styles}>
+    </div>
+
+  </div>
+  )
+};
+
+function Msg() {
+  return (
+    <h2 className="text-center home"> Welcome to  My app 🎬🎮✨⚡</h2>
+  )
+}
+
---
+++ src/TicTacToe.js
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import Button from '@mui/material/Button';
+import "./tictactoe.css";
+export function TicTacToe() {
+
+
+    const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null]);
+
+    // const [board,setBoard]=useState([0,1,2,3,4,5,6,7,8])
+    const decideWinner = (board) => {
+
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (var i in lines) {
+
+            const [a, b, c] = lines[i];
+            // console.log(a,b,c)
+            if (board[a] !== null && board[a] === board[b] && board[b] === board[c]) {
+                return board[a];
+            }
+
+            // console.log(`winner is ${board[a]}`)
+        }
+
+        return null;
+
+    };
+
+
+
+    const winner = decideWinner(board);
+
+
+
+    const draw = (board) => {
+        if (!board.includes(null) && winner === null) {
+            return "draw";
+        }
+    };
+    const drawn = draw(board);
+
+
+
+    const [isxturn, setXTurn] = useState(true);
+    // const xO = isxturn ? "X" : "O";
+    const handleClick = (index) => {
+        if (winner === null && board[index] === null) {
+            const boardCopy = ([...board]);
+
+            boardCopy[index] = isxturn ? "X" : "O";
+            setBoard(boardCopy);
+            setXTurn(!isxturn);
+        }
+    };
+
+    const btnstyle = {
+        display: board[0] || board[1] || board[2] || board[2] || board[3] || board[4] || board[5] || board[6] || board[7] || board[8] !== null ? "none" : "block"
+    }
+
+
+
+    return (<div className="full-game">
+        <div style={btnstyle} className="player-select-button">
+            <Button variant="outlined" onClick={() => setXTurn(true)}>Player X</Button>
+            <Button variant="outlined" onClick={() => setXTurn(false)}>Player O</Button>
+
+        </div>
+        <span className={winner || drawn ? "playerturnhide" : "playerturnshow"}>{isxturn ? <h2>X Turn🎮</h2> : <h2>O Turn🎮</h2>}</span>
+        <div className="board">
+            {board.map((val, index) => <GameBox val={val} key={index} onPlayerClick={() => handleClick(index)} />)}
+            {/* <GameBox /> */}
+        </div>
+        {winner ? <h2>Winner is: 👏{winner}</h2> : ""}
+
+        {drawn ? <h2>Match : 🤝{drawn}</h2> : ""}
+
+        <div className="restart-btn"><Button variant="contained" onClick={() => setBoard([null, null, null, null, null, null, null, null, null])}>Restart</Button></div>
+
+    </div>
+    );
+}
+function GameBox({ val, onPlayerClick }) {
+    // const val="x"
+
+    // const [val,setVal]=useState(null)
+    const sytles = {
+        color: val === 'X' ? "green" : "red"
+    };
+
+    return (
+        <div
+            onClick={() => onPlayerClick()}
+            style={sytles}
+            className="game-box"
+
+        >
+            {val}
+        </div>
+    );
+}
